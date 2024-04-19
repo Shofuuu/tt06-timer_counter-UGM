216b9889496a8a83c032131f0725a70de96b2a66
Muh. Shofuwan Anwar <44674870+Shofuuu@users.noreply.github.com>
2024-04-19 22:59:56 +0700

    Fix port type error (input to output)

--- src/tt_um_timer_counter_UGM.v
+++ src/tt_um_timer_counter_UGM.v
@@ -2,7 +2,7 @@
 
 module tt_um_timer_counter_UGM (
     input wire [7:0] ui_in, // Dedicated inputs - connected to the 8-bit ADC parallel output
-    input wire [7:0] uo_out, // Dedicated outputs - connected to adc convStart, adc rd_cs, and synchronous rectifier high and low
+    output wire [7:0] uo_out, // Dedicated outputs - connected to adc convStart, adc rd_cs, and synchronous rectifier high and low
     input  wire [7:0] uio_in,   // IOs: Bidirectional Input path - one input connected to adc busy signal
     output wire [7:0] uio_out,  // IOs: Bidirectional Output path
     output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
